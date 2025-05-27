1e2dcb1a361a4fadadfd4db1da1acc36ce3c7207
MitzukiM <johndoe@example.com>
2025-05-27 19:59:36 +0300

    setup bootstrap

--- frontend/app/router/main_page_router.py
+++ frontend/app/router/main_page_router.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from http.client import responses
+from lib2to3.fixes.fix_input import context
 
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 router = APIRouter()
 
+templates = Jinja2Templates(directory='templates')
+
+
 @router.get('/')
-async def index():
-    return 'mitzufdkl'
+async def index(request : Request):
+    context = {'request':request}
+    response = templates.TemplateResponse('index.html',context=context)
+    return response
 
