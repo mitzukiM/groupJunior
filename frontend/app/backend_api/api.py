faa2c40fa30b54e305b47370b95db44308143f37
MitzukiM <johndoe@example.com>
2025-06-03 21:41:31 +0300

    user

--- frontend/app/backend_api/api.py
+++ frontend/app/backend_api/api.py
@@ -6,23 +6,38 @@ from fastapi import Request
 async def login_user(user_email: str, password: str):
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            url=f'{settings.BACKEND_API}auth/login',
-            data={"username": user_email, 'password': password}
+            url=f"{settings.BACKEND_API}auth/login",
+            data={"username": user_email, "password": password},
         )
-        print(response.json())
         return response.json()
+
+
+async def register_user(user_email: str, password: str, name: str):
+    async with httpx.AsyncClient() as client:
+        print(settings.BACKEND_API,999999999999999999999999999)
+        response = await client.post(
+            url=f"{settings.BACKEND_API}users/create",
+            json={"name": name, "password": password, "email": user_email},
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json(), 8888888888888888888)
+        return response.json()
+
+
 async def get_user_info(access_token: str):
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            url=f'{settings.BACKEND_API}auth/get-my-info',
-            headers={"Authorization": f'Bearer {access_token}'}
+            url=f"{settings.BACKEND_API}auth/get-my-info",
+            headers={"Authorization": f"Bearer {access_token}"},
         )
         print(response.json())
         return response.json()
+
+
 async def get_current_user_with_token(request: Request) -> dict:
-    access_token = request.cookies.get('access_token')
+    access_token = request.cookies.get("access_token")
     if not access_token:
         return {}
     user = await get_user_info(access_token)
-    user['access_token'] = access_token
+    user["access_token"] = access_token
     return user
