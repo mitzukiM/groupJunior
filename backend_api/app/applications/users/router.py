87f7d1fda3288814f593762dce019446b48b7b9f
MitzukiM <johndoe@example.com>
2025-06-06 20:47:31 +0300

    save user

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,26 +1,32 @@
+import uuid
 
-
-from fastapi import APIRouter, Depends, status , HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from applications.users.crud import create_user_in_db, get_user_by_email
-from applications.users.schemas import BaseFields, RegisterUserField
+from applications.users.crud import create_user_in_db, get_user_by_email, activate_user_account
+from applications.users.schemas import BaseUserInfo, RegisterUserField
 from database.session_dependencies import get_async_session
 
 router_users = APIRouter()
 
 
-
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserField, session: AsyncSession = Depends(get_async_session)) -> BaseFields:
+async def create_user(
+    new_user: RegisterUserField, session: AsyncSession = Depends(get_async_session)
+) -> BaseUserInfo:
     user = await get_user_by_email(new_user.email, session)
-    if user :
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Already exists')
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="Already exists"
+        )
 
-    created_user = await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
+    created_user = await create_user_in_db(
+        new_user.email, new_user.name, new_user.password, session
+    )
 
+    return created_user
 
-
-
-
-    return new_user
+@router_users.get('/verify/{user_uuid}')
+async def verify_user(user_uuid:uuid.UUID,session: AsyncSession = Depends(get_async_session)):
+    await activate_user_account(user_uuid,session)
+    return {"Status":"activated"}
